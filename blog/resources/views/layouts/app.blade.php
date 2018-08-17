e5a5b407633fe76e7fb863480a5b6837743039d8
acbrent25 <adamchampagne@gmail.com>
2018-08-17 07:45:26 -0400

    made middleware to filter content per user role

--- blog/resources/views/layouts/app.blade.php
+++ blog/resources/views/layouts/app.blade.php
@@ -83,12 +83,15 @@
                   <li class="list-group-item">
                     <a href="{{ route('home') }}">Home</a>
                   </li>
-                  <li class="list-group-item">
-                    <a href="{{ route('users') }}">Users</a>
-                  </li>
-                  <li class="list-group-item">
-                    <a href="{{ route('user.create') }}">New User</a>
-                  </li>
+
+                  @if(Auth::user()->admin)
+                    <li class="list-group-item">
+                      <a href="{{ route('users') }}">Users</a>
+                    </li>
+                    <li class="list-group-item">
+                      <a href="{{ route('user.create') }}">New User</a>
+                    </li>
+                  @endif
                   <li class="list-group-item">
                     <a href="{{ route('categories') }}">Categories</a>
                   </li>
